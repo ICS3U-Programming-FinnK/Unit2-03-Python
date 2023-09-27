18064515d7f6217744b5c78b852f494ba2bd2d7c
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-09-27 21:27:08 +0000

    Added circ_circle_tau.py

---
+++ circ_circle_tau.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Created by: Finn Kitor
+# Created on: September 27th, 2023
+# This program asks the user for the radius of
+# a circle in mm. It then calculates and displays
+# the circumference using tau.
+import constants
+
+
+def main():
+    # user imputs the radius into the terminal
+    radius = float(input("Enter the radius of the circle (cm): "))
+
+    # terminal calculates the circumference
+    circumference = constants.TAU * radius
+
+    # terminal displays the circumference
+    print("")
+    print("Circumference = {} cm".format(circumference))
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created by: Finn Kitor
+# Created on: September 27th, 2023
+# This module contains constants.
+
+TAU = 6.28
